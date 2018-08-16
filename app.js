8482829cc90323faba41d469a70dcc7cd24b0b34
eaw20024 <eric.washington.me@gmail.com>
2018-08-16 14:48:53 -0400

    app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+// Init github
+const github = new Github;
+
+// Init UI
+const ui = new UI;
+
+// Search Input
+const searchUser = document.getElementById('searchUser');
+
+// Search Input Event Listener
+
+searchUser.addEventListener('keyup', (e) => {
+    // Get Input Text
+    const userText = e.target.value;
+
+    if(userText !== ''){
+            //Make HTTP Call
+            github.getUser(userText)
+            .then(data => {
+                if(data.profile.message === 'Not Found'){
+                    // Show Alert
+                    ui.showAlert('User not found', 'alert alert-danger')
+                } else {
+                    // Show Profile
+                    ui.showProfile(data.profile);
+                    ui.showRepos(data.repos)
+                }   
+            })
+    } else { 
+        // Clear Profile
+        ui.clearProfile();
+    }
+});
